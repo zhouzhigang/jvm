cb850ea6e9d36f3972e9a1136a1a0514f118355c
Zhigang Zhou <zhouzhigang90s@gmail.com>
2015-09-08 19:59:24 +0800

    Getting and Setting Field Values - Filed Reflection
    f = c.getDeclaredFiled(fieldName);f.set*(instance, value); f.get(instance);

---
+++ reflection/members/Book.java
@@ -0,0 +1,43 @@
+import java.lang.reflect.Field;
+import java.util.Arrays;
+import static java.lang.System.out;
+
+enum Tweedle { DEE, DUM }
+
+/**
+ * Getting and Setting Field Values.
+ */
+public class Book {
+    public long chapters = 0;
+    public String[] characters = { "Alice", "White Rabbit" };
+    public Tweedle twin = Tweedle.DEE;
+
+    public static void main(String[] args) {
+        Book book = new Book();
+        String fmt = "%6S:  %-12s = %s%n";
+
+        try {
+            Class<?> c = book.getClass();
+            Field chap = c.getDeclaredField("chapters");
+            out.format(fmt, "before", "chapters", book.chapters);
+            chap.setLong(book, 12);
+            out.format(fmt, "after", "chapters", chap.getLong(book));
+
+            Field chars = c.getDeclaredField("characters");
+            out.format(fmt, "before", "characters", Arrays.asList(book.characters));
+            String[] newChars = { "Queue", "King"};
+            chars.set(book, newChars);
+            out.format(fmt, "after", "characters", Arrays.asList(book.characters));
+
+            Field t = c.getDeclaredField("twin");
+            out.format(fmt, "before", "twin", book.twin);
+            t.set(book, Tweedle.DUM);
+            out.format(fmt, "after", "twin", t.get(book));
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
--- reflection/members/README.md
+++ reflection/members/README.md
@@ -13,7 +13,7 @@ A `field` is a class, interface, or enum iwth an associated value.
     f.getType();
     f.getGenericType();
 
-e.g. [FieldSpy.java](FieldSpy/java)
+e.g. [FieldSpy.java](FieldSpy.java)
 
 ### Retrieving and Parsing Field Modifiers
 
@@ -36,6 +36,18 @@ e.g. [FieldModifierSpy.java](FieldModifierSpy.java)
 
 
 ### Getting and Setting Field Values
+
+    Field f = c.getDeclaredFiled("fielName");
+    f.setLong(instance, value);
+    f.getLong(instance);
+    f.set(instance, value);
+    f.get(instance);
+
+Note: Setting a field's value via reflection has a certain amount of performance overhead because various operations must occur such as validating access permissions.
+
+Use of reflection can cause some runtime optimizations to be lost. For example, `int x = 1; x = 2; x =3;` will be optimized by JVM, while using `Field.set*()` will not.
+
+e.g [Book.java](Book.java)
 
 ### Troubleshooting
 
